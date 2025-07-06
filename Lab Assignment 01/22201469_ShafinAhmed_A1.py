642b6f8ec7090ca55846fa320b3e75bd2312e03f
Shafin Ahmed <shafin.armstrong5@gmail.com>
2025-07-06 06:46:58 +0600

    Input taking successful for part 1

--- Lab Assignment 01/22201469_ShafinAhmed_A1.py
+++ Lab Assignment 01/22201469_ShafinAhmed_A1.py
@@ -1,7 +1,18 @@
 # Part 01
 def run_part1(filename):
     with open(filename, 'r') as file1:
+        count=0
+        matrix=[]
         for line in file1:
-            print(line)
+            if count==0:
+                n,m=map(int,line.split())
+            elif count==1:
+                a,b=map(int,line.split())
+            elif count==2:
+                c,d=map(int,line.split())
+            else:
+                matrix+=[list(line.split())]
+            count+=1
+    print(n,m,a,b,c,d,matrix)
             
 run_part1('test_input1.txt')
