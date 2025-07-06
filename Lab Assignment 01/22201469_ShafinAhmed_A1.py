2fb11a32976f6be191a5c08d1d53d29cf7f20267
Shafin Ahmed <shafin.armstrong5@gmail.com>
2025-07-06 07:01:36 +0600

    Implemented heuristic function

--- Lab Assignment 01/22201469_ShafinAhmed_A1.py
+++ Lab Assignment 01/22201469_ShafinAhmed_A1.py
@@ -1,4 +1,16 @@
 # Part 01
+import heapq
+def heuristic(j,k):
+    return abs(j[0]-k[0])+abs(j[1]-k[1])
+
+def a_star(matrix,start,goal):
+    r,c=len(matrix),len(matrix[0])
+    visited=set()
+    parent={}
+    g_n={start:0}
+    f_n={start: heuristic(start,goal)}
+    
+
 def run_part1(filename):
     with open(filename, 'r') as file1:
         count=0
@@ -13,6 +25,12 @@ def run_part1(filename):
             else:
                 matrix+=[list(line.split())]
             count+=1
-    print(n,m,a,b,c,d,matrix)
-            
+    cost,seq=a_star(matrix,(a,b),(c,d))
+    print(cost)
+    print(seq)
+
+#As we were instructed the program must read from input text file
+#But is the assignment folder we were given a pdf file
+#I have created a test file for myself
+#and the program runs for any text input; just need to edit the text file name on the run_part1() function            
 run_part1('test_input1.txt')
