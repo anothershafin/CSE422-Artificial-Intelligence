8f77d8bc014e232197943d1959fd1c0659b07993
Shafin Ahmed <shafin.armstrong5@gmail.com>
2025-08-14 03:48:58 +0600

    Wiring Distance function running successfully

--- Lab Assignment 02/22201469_ShafinAhmed_A2.py
+++ Lab Assignment 02/22201469_ShafinAhmed_A2.py
@@ -10,8 +10,13 @@ components = [
     ("Floating Unit", 5, 5)
 ]
 
+connections = [
+    (3, 0), (2, 0), (0, 1),
+    (3, 5), (1, 4), (4, 5)
+]
+
 totalArea=25
-iterations=15
+iterations=5
 
 def generate_chromosome():
     return [(random.randint(0,totalArea), random.randint(0,totalArea)) for x in range(len(components))]
@@ -20,7 +25,6 @@ def calc_overlaps(chromosome):
     count = 0
     for i in range(len(chromosome)):
         for j in range(i+1, len(chromosome)):
-            print(chromosome[i],chromosome[j])
             #counting bottom left index of the two components
             aleftx=chromosome[i][0]
             alefty=chromosome[i][1]
@@ -33,12 +37,27 @@ def calc_overlaps(chromosome):
             bup=blefty+components[j][2]
 
             if not (aright<=bleftx) or (aleftx>=bright) or (aup<=blefty) or (alefty>=bup):
-                count+=1
-    print(f"count={count}")      
+                count+=1    
     return count
+
+def calc_wd(chromosome):
+    total=0
+    for i,j in connections:
+        print(f"ch:{chromosome[i]} {chromosome[j]}")
+        #calculating centers
+        a_centerx=chromosome[i][0]+(components[i][1]/2)
+        a_centery=chromosome[i][1]+(components[i][2]/2)
+        b_centerx=chromosome[j][0]+(components[j][1]/2)
+        b_centery=chromosome[j][1]+(components[j][2]/2)
+        
+        total+=math.sqrt((a_centerx-b_centerx)**2+(a_centery-b_centery)**2)
+        print(f"wd:{total}")
+    
+    return total
 
 def fitness(chromosome):
     overlap=calc_overlaps(chromosome)
+    wiring=calc_wd(chromosome)
 
 
 population=[generate_chromosome() for y in range(6)]
